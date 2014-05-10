d5e423f24ababe319362cdde8913ceb0bc804e56
agatov <agatovs@gmail.com>
2014-05-10 19:59:36 +0400

    guest model added

---
+++ db/migrate/20140510155702_create_guests.rb
@@ -0,0 +1,12 @@
+class CreateGuests < ActiveRecord::Migration
+  def change
+    create_table :guests do |t|
+      t.string :name
+      t.string :phone
+      t.integer :card_code
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
